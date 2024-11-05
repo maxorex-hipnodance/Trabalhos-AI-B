4a969ce40bc65842b2a50178f60877e65f17daf6
maxorex <maxorextop@gmail.com>
2024-11-05 17:01:52 +0000

    Update(1) calculator 2.py
    adicionei historia da calcucacao completa

--- calculator 2.py
+++ calculator 2.py
@@ -38,32 +38,32 @@ def calculadora():
     if a == '+':
         result = soma(float(num1), float(num2))
         print('Result of amount: ' + str(result))
-        history.insert(0,result)
+        history.insert(0,f'{num1} {a} {num2} = {result}')
 
     if a == '-':
         result1 = subtraçao(float(num1), float(num2))
         print('Result of subtraction: ' + str(result1))
-        history.insert(0, result1)
+        history.insert(0, f'{num1} {a} {num2} = {result1}')
 
     if a == '/':
         result2 = divisao(float(num1), float(num2))
         print('Result of division: ' + str(result2))
-        history.insert(0, result2)
+        history.insert(0, f'{num1} {a} {num2} = {result2}')
 
     if a == '*':
         result3 = multiplicaçao(float(num1), float(num2))
         print('Result of multiplication: ' + str(result3))
-        history.insert(0, result3)
+        history.insert(0, f'{num1} {a} {num2} = {result3}')
 
     if a != '+' and a != '-' and a != '*' and a != '/':
-        print('!ALER!  try again The problem in +, -, *, or / ... ')
+        print('!ALER!⚠️try again The problem in +, -, *, or / ...')
         calculadora()
     else: pass
 
     if len(history) >= 6:
         history.pop(5)
 
-    e = input(" Show history? yes/no: ")
+    e = input(" Show history?  yes/no: ")
     if e == 'yes' or e == 'Yes' or e == 'YES':
         for record in history:
             print(record)
@@ -72,7 +72,7 @@ def calculadora():
 
 
 
-    b = input("Stop? yes/no: ")
+    b = input(" Stop? yes/no: ")
     if b == 'yes' or b == 'Yes' or b == 'YES':
         shutdown()
     else:
